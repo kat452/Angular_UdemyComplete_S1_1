0dfc4afd1443008d828007d55355e2eab3e794a5
Katelyn <katelyn.kuntz@yash.com>
2023-02-08 11:43:49 -0600

    ngIf with else condition

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit{
+  allowNewServer =false;
+  serverCreationStatus='Nope'
+  serverName = '';
+  serverCreated=false;
+  constructor(){
+      setTimeout(()=> {
+        this.allowNewServer=true
+      },2000)
+  }
+  ngOnInit(): void {
+  }
+  onCreateServer(){
+    this.serverCreationStatus="server was created name is "+this.serverName
+    this.serverCreated=true;
+  }
 
+  onUpdateServerName(event:Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
+    console.log(event)
+  }
 }
